718114fd65d1078a8b696565c80ae4271e3f5c73
Muhammed Moussa <muhammedmoussa@hotmail.com>
2019-07-03 23:43:52 +0200

    added basic get function

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,20 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+admin.initializeApp();
+
 exports.helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from ReacFire!");
+});
+
+exports.getScreams = functions.https.onRequest((request, response) => {
+    admin
+    .firestore()
+    .collection('screams')
+    .then(data => {
+        let screams = [];
+        data.forEach( doc => screams.push(doc.data()));
+        return response.json(screams);
+    })
+    .catch(error => console.error(error));
 });
