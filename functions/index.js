1e4329107d78825c62d6f60ec75dac6a3e836134
Muhammed Moussa <muhammedmoussa@hotmail.com>
2019-07-06 00:37:48 +0200

    signup validation

--- functions/index.js
+++ functions/index.js
@@ -46,12 +46,32 @@ app.post('/screams', (request, response) => {
 
 // Signup Route
 app.post('/signup', (request, response) => {
+    debugger
     const newUser = {
         email: request.body.email,
         password: request.body.password,
         confirmPassword: request.body.confirmPassword,
         handle: request.body.handle,
     };
+
+    let errors = {};
+
+    if (isEmpty(newUser.email)) {
+        errors.email = 'Must not be empty';
+    } else if (!isEmail(newUser.email)) {
+        errors.email = 'Must be a valid email address';
+    }
+
+    if (isEmpty(newUser.password)) { errors.password = 'Must not be empty'; }
+    if (newUser.password
+            && newUser.confirmPassword
+                && newUser.password !== newUser.confirmPassword) {
+                    errors.confirmPassword = 'Passwords must match';
+    }
+
+    if (isEmpty(newUser.handle)) { errors.handle = 'Must not be empty'; }
+
+    if (Object.keys(errors).length > 0) { return response.status(400).json(errors); }
 
     let token, userId;
 
@@ -93,5 +113,17 @@ app.post('/signup', (request, response) => {
     })
     // @TODO: Validate Data
 });
+
+// Validations Helpers..
+const isEmail = (email) => {
+    const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (email.match(regEx)) return true;
+    else return false;
+};
+
+const isEmpty = (string) => {
+    if (string.trim() === '') return true;
+    else return false;
+};
 
 exports.api = functions.region('europe-west1').https.onRequest(app);
