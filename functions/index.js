a1930786450d0f7253a6683516ad2131e23de028
Muhammed Moussa <muhammedmoussa@hotmail.com>
2019-07-06 00:03:15 +0200

    update gitignore

--- functions/index.js
+++ functions/index.js
@@ -3,18 +3,10 @@ const admin = require('firebase-admin');
 const firebase = require('firebase');
 const app = require('express')();
 
-const firebaseConfig = {
-    apiKey: "AIzaSyBbDjjAE1vbUwtGnuVBGwMU9dtRAP8enTY",
-    authDomain: "reactfire-fb6d1.firebaseapp.com",
-    databaseURL: "https://reactfire-fb6d1.firebaseio.com",
-    projectId: "reactfire-fb6d1",
-    storageBucket: "reactfire-fb6d1.appspot.com",
-    messagingSenderId: "362775888219",
-    appId: "1:362775888219:web:3a6e10fbfe5644e7"
-  };
+const config = require('./config');
 
 admin.initializeApp();
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(config.firebaseConfig);
 const db = admin.firestore();
 
 app.get('/screams', (request, response) => {
