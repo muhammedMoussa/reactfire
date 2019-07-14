3b9e7e2da67a880d7bb4ac9cd31b723b1d170635
Muhammed Moussa <muhammedmoussa@hotmail.com>
2019-07-15 00:22:59 +0200

    fix logs error

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,7 +1,7 @@
 const functions = require('firebase-functions');
 const app = require('express')();
 
-const db = require('./util/admin');
+const { db } = require('./util/admin');
 const { getAllScreams,
         postOneScream,
         getScream,
