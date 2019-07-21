a24c38f1e951db6bfedbe088979b46030810ff52
Muhammed Moussa <muhammedmoussa@hotmail.com>
2019-07-22 00:38:09 +0200

    update

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -97,22 +97,24 @@ exports.createNotificationOnComment = functions
 });
 
 exports.onUserImageChange = functions
-.region('europe-west1')
-.firestore.document('/users/{userId}')
-.onUpdate(change => {
-  console.log(change.before.data());
-  console.log(change.after.data());
-  if (change.before.data().imageUrl !== change.after.data().imageUrl) {
-    let batch = db.batch();
-    return db.collection('screams').where('userHandle', '==', change.before.userHandle).get()
-    .then(data => {
-      data.forEach(doc => {
-        const scream = db.doc(`/screams/${doc.id}`);
-        batch.update(scream, { userImage: change.after.imageUrl });
-      });
-      batch.commit();
-    });
-  }
+  .region('europe-west1')
+  .firestore.document('/users/{userId}')
+  .onUpdate((change) => {
+    if (change.before.data().imageUrl !== change.after.data().imageUrl) {
+      console.log('image has changed');
+      const batch = db.batch();
+      return db
+        .collection('screams')
+        .where('userHandle', '==', change.before.data().handle)
+        .get()
+        .then((data) => {
+          data.forEach((doc) => {
+            const scream = db.doc(`/screams/${doc.id}`);
+            batch.update(scream, { userImage: change.after.data().imageUrl });
+          });
+          return batch.commit();
+        });
+    } else return true;
 });
 
 exports.onScreamDelete = functions
