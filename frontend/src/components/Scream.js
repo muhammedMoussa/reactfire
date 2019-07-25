ef94633bd9d390c5f2b2ed4456bf35c3f68bd211
Muhammed Moussa <muhammedmoussa@hotmail.com>
2019-07-26 01:16:27 +0200

    added client  login page

--- frontend/src/components/Scream.js
+++ frontend/src/components/Scream.js
@@ -6,7 +6,6 @@ import Moment from 'moment';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import Typorgraphy from '@material-ui/core/Typography';
 import Typography from '@material-ui/core/Typography';
 
 const styles = {
@@ -46,14 +45,14 @@ class Scream extends Component {
         className={classes.image}
       />
       <CardContent className={classes.content}>
-        <Typorgraphy
+        <Typography
           variant="h5"
           component={Link}
           to={`/users/${userHandle}`}
           color="primary"
         >
           { userHandle }
-        </Typorgraphy>
+        </Typography>
         <Typography variant="body2" color="textSecondary">
           { Moment(createdAt).fromNow() }
         </Typography>
