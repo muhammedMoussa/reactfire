7797c5835dd2954b37b9ab7eb696b6bf89057ebb
Muhammed Moussa <muhammedmoussa@hotmail.com>
2019-08-10 15:46:30 +0200

    project restructure ♻️

---
+++ server/functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apiKey: "AIzaSyBbDjjAE1vbUwtGnuVBGwMU9dtRAP8enTY",
+    authDomain: "reactfire-fb6d1.firebaseapp.com",
+    databaseURL: "https://reactfire-fb6d1.firebaseio.com",
+    projectId: "reactfire-fb6d1",
+    storageBucket: "reactfire-fb6d1.appspot.com",
+    messagingSenderId: "362775888219",
+    appId: "1:362775888219:web:3a6e10fbfe5644e7"
+};
+
